5dd54d1ff350fb874f77a2f3c4de78b878f08d21
marietheresewambele-droid <marietheresewambele@gmail.com>
2026-08-11 00:14:21 +0100

    Add bioenergetics Biology route

---
+++ src/app/biology/bioenergetics/page.tsx
@@ -0,0 +1,6 @@
+import BiologyTopicPage from "@/components/topic/BiologyTopicPage";
+import { bioenergeticsConfig } from "@/data/topics/bioenergetics";
+
+export default function BioenergeticsPage() {
+  return <BiologyTopicPage config={bioenergeticsConfig} />;
+}
