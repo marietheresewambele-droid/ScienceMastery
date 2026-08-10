b958995f81e2a8f40ba6d5f0a60fb2a70d57a254
marietheresewambele-droid <marietheresewambele@gmail.com>
2026-08-11 00:48:08 +0100

    Add Chemistry mastery content: src/app/chemistry/chemical-analysis/page.tsx

---
+++ src/app/chemistry/chemical-analysis/page.tsx
@@ -0,0 +1,3 @@
+import BiologyTopicPage from "@/components/topic/BiologyTopicPage";
+import { chemicalAnalysisConfig } from "@/data/topics/chemical-analysis";
+export default function ChemicalAnalysisPage(){ return <BiologyTopicPage config={chemicalAnalysisConfig} />; }
