4c7c8188f102d0a59f170914bf5ea3b1dcd93693
marietheresewambele-droid <marietheresewambele@gmail.com>
2026-08-11 00:14:13 +0100

    Add organisation Biology route

---
+++ src/app/biology/organisation/page.tsx
@@ -0,0 +1,6 @@
+import BiologyTopicPage from "@/components/topic/BiologyTopicPage";
+import { organisationConfig } from "@/data/topics/organisation";
+
+export default function OrganisationPage() {
+  return <BiologyTopicPage config={organisationConfig} />;
+}
