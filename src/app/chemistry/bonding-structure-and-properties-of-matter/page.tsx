3d9620ef0da0ac6b8ca02482280abd3e9de4b49b
marietheresewambele-droid <marietheresewambele@gmail.com>
2026-08-11 00:46:56 +0100

    Add Chemistry mastery content: src/app/chemistry/bonding-structure-and-properties-of-matter/page.tsx

---
+++ src/app/chemistry/bonding-structure-and-properties-of-matter/page.tsx
@@ -0,0 +1,3 @@
+import BiologyTopicPage from "@/components/topic/BiologyTopicPage";
+import { bondingStructureAndPropertiesOfMatterConfig } from "@/data/topics/bonding-structure-and-properties-of-matter";
+export default function BondingStructureAndPropertiesOfMatterPage(){ return <BiologyTopicPage config={bondingStructureAndPropertiesOfMatterConfig} />; }
