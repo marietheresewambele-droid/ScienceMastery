d73e14d5c594574493cce6906607f73e36a34189
marietheresewambele-droid <marietheresewambele@gmail.com>
2026-08-11 00:47:08 +0100

    Add Chemistry mastery content: src/app/chemistry/quantitative-chemistry/page.tsx

---
+++ src/app/chemistry/quantitative-chemistry/page.tsx
@@ -0,0 +1,3 @@
+import BiologyTopicPage from "@/components/topic/BiologyTopicPage";
+import { quantitativeChemistryConfig } from "@/data/topics/quantitative-chemistry";
+export default function QuantitativeChemistryPage(){ return <BiologyTopicPage config={quantitativeChemistryConfig} />; }
