e3c309c5ac88e454590aa8408bae66f31cd46653
marietheresewambele-droid <marietheresewambele@gmail.com>
2026-08-11 00:14:27 +0100

    Add inheritance-variation-and-evolution Biology route

---
+++ src/app/biology/inheritance-variation-and-evolution/page.tsx
@@ -0,0 +1,6 @@
+import BiologyTopicPage from "@/components/topic/BiologyTopicPage";
+import { inheritanceVariationAndEvolutionConfig } from "@/data/topics/inheritance-variation-and-evolution";
+
+export default function InheritanceVariationAndEvolutionPage() {
+  return <BiologyTopicPage config={inheritanceVariationAndEvolutionConfig} />;
+}
