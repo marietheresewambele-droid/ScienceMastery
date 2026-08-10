7f95b53d2ee8b972b180b82707f680a66e2ae5b4
marietheresewambele-droid <marietheresewambele@gmail.com>
2026-08-11 00:48:22 +0100

    Add Chemistry mastery content: src/app/chemistry/chemistry-of-the-atmosphere/page.tsx

---
+++ src/app/chemistry/chemistry-of-the-atmosphere/page.tsx
@@ -0,0 +1,3 @@
+import BiologyTopicPage from "@/components/topic/BiologyTopicPage";
+import { chemistryOfTheAtmosphereConfig } from "@/data/topics/chemistry-of-the-atmosphere";
+export default function ChemistryOfTheAtmospherePage(){ return <BiologyTopicPage config={chemistryOfTheAtmosphereConfig} />; }
