c92c01b9f29e1a5490c134f04a1072068b077bd5
marietheresewambele-droid <marietheresewambele@gmail.com>
2026-08-11 00:14:24 +0100

    Add homeostasis-and-response Biology route

---
+++ src/app/biology/homeostasis-and-response/page.tsx
@@ -0,0 +1,6 @@
+import BiologyTopicPage from "@/components/topic/BiologyTopicPage";
+import { homeostasisAndResponseConfig } from "@/data/topics/homeostasis-and-response";
+
+export default function HomeostasisAndResponsePage() {
+  return <BiologyTopicPage config={homeostasisAndResponseConfig} />;
+}
