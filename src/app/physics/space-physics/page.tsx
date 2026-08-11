c118a1bb5f1f6558b338982847b96cce7ecaae5b
marietheresewambele-droid <marietheresewambele@gmail.com>
2026-08-11 18:39:39 +0100

    Add Physics mastery topics

---
+++ src/app/physics/space-physics/page.tsx
@@ -0,0 +1,3 @@
+import BiologyTopicPage from "@/components/topic/BiologyTopicPage";
+import { physicsSpacePhysicsConfig } from "@/data/topics/physics-space-physics";
+export default function PhysicsTopicPage(){ return <BiologyTopicPage config={physicsSpacePhysicsConfig} />; }
