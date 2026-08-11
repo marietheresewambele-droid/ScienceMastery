4d4fbdffbea3522ccd8639b8c7c47aea2ce37f07
marietheresewambele-droid <marietheresewambele@gmail.com>
2026-08-11 18:38:53 +0100

    Add Physics mastery topics

---
+++ src/app/physics/particle-model-of-matter/page.tsx
@@ -0,0 +1,3 @@
+import BiologyTopicPage from "@/components/topic/BiologyTopicPage";
+import { physicsParticleModelOfMatterConfig } from "@/data/topics/physics-particle-model-of-matter";
+export default function PhysicsTopicPage(){ return <BiologyTopicPage config={physicsParticleModelOfMatterConfig} />; }
